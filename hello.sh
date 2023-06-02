bf6a263fdfc3ece01f1457e6fb0bd75e6ba8a017
Mayur <mayur143@mayurubuntu2022.iballbatonwifi.com>
2023-06-02 18:38:09 +0530

    This Is My First Hello World Program

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+
+# this program for printing a hello world but it's modified to understand the echo command we used echo multiple times to print multiple Outputs
+
+echo "my name is anthony gonsal vees  woooohh woooooohhhh" 
+echo "me duniya me akela hu"
+echo "dil bhi hai khali"
+echo "ghar bhi he khali"
+echo "isme rahegi"
+echo "koi kismat wali"
+
